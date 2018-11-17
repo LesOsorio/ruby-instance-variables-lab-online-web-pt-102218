127304a60e9104abd870791b14812852ffc50a80
Leslie Osorio <lesosorio17@gmail.com>
2018-11-17 02:45:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
 
   def name
@@ -9,4 +9,4 @@ class Dog
   end
 end
 
-lassie = Dog.new 
+lassie = Dog.new
